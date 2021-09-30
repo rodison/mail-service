caa5e01ba5bf7d16556700e4a0c8e7c1dff5e3d9
rodisonferreira <rodison.ferreira@jexperts.com.br>
2021-09-29 22:04:39 -0300

    feat(mail): jooq generator

--- src/main/java/br/com/rodison/mailservice/GreetingResource.java
+++ src/main/java/br/com/rodison/mailservice/GreetingResource.java
@@ -1,9 +1,12 @@
-package br.com.rodison.supportservices;
+package br.com.rodison.mailservice;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import br.com.rodison.mailservice.db.jooq.generated.tables.MailAttachment;
+import br.com.rodison.mailservice.db.jooq.generated.tables.MailCc;
 
 @Path("/hello")
 public class GreetingResource {
@@ -11,6 +14,8 @@ public class GreetingResource {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String hello() {
+		new MailCc();
+		new MailAttachment();
 		return "Hello RESTEasy";
 	}
 }
